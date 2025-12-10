e4c7e3134a6c833ca6a4c470e36543dbd098d68e
Matthew Shaxted <mattshax@gmail.com>
2025-12-10 10:14:44 -0600

    Fix resolve (#4)
    * Fixing run.sh alignment
    
    * Separate lib.sh script.
    
    * fixing run issues.

--- run.sh
+++ run.sh
@@ -19,12 +19,7 @@ else
     echo "User does NOT have sudo privileges"
 
     source lib.sh
-<<<<<<< HEAD
-    start_rootless_docker()
-
-=======
     start_rootless_docker
->>>>>>> origin/main
     docker_cmd="docker"
 
 fi
